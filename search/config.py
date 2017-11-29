3b7d21a0a566f4930cca53eb5a3b665e456efd9a
Tamox <jake.cui2@gmail.com>
2017-11-28 21:05:21 -0500

    asdf

---
+++ search/config.py
@@ -0,0 +1,10 @@
+#--------------------------------------------------------------------------------
+# These tokens are needed for user authentication.
+# Credentials can be generates via Twitter's Application Management:
+#	https://apps.twitter.com/app/new
+#--------------------------------------------------------------------------------
+
+consumer_key = "ZfUELe0dim4V4ShuRgZn0d0uf"
+consumer_secret = "TRH9gBX14FS3VPfi0ZOzv0UELTU1CVJ5eU6AneXGiB3GA0ImNr"
+access_key = "1557147361-JhGmrvy7DORavdek1oCYVDuwJtS43TU3iWard8Q"
+access_secret = "FbvZ7urp2opnlaSDV4ZdFJ6HQoyA0UqPLIKZcQZldPm1q"
