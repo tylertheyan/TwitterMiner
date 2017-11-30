316db864ddb1fead010c5df42a3cc831fd293cca
Jenny Huang <jhuange6@gmail.com>
2017-11-29 23:42:09 -0500

    Added list of positive names and phrases

--- search/twitter_search.py
+++ search/twitter_search.py
@@ -18,25 +18,37 @@ execfile("config.py", config)
 # create twitter API object
 #-----------------------------------------------------------------------
 twitter = Twitter(
-		        auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
+                auth = OAuth(config["access_key"], config["access_secret"], config["consumer_key"], config["consumer_secret"]))
 
+depression_phrases = [
+    "I have depression",
+    "I use antidepressants",
+    "I was diagnosed with depression"
+    ]
 
-#-----------------------------------------------------------------------
-# perform a basic search 
-# Twitter API docs:
-# https://dev.twitter.com/rest/reference/get/search/tweets
-#-----------------------------------------------------------------------
-query = twitter.search.tweets(q='I am depressed', result_type='recent', lang='en', count=1000)
+names = open('positives_names', 'w')
+count = 0
+for phrase in depression_phrases:
+    
+    #-----------------------------------------------------------------------
+    # perform a basic search 
+    # Twitter API docs:
+    # https://dev.twitter.com/rest/reference/get/search/tweets
+    #-----------------------------------------------------------------------
+    query = twitter.search.tweets(q=phrase, result_type='recent', lang='en', count=100)
 
-#-----------------------------------------------------------------------
-# How long did this query take?
-#-----------------------------------------------------------------------
-print "Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"])
+    #-----------------------------------------------------------------------
+    # How long did this query take?
+    #-----------------------------------------------------------------------
+    print "Search complete (%.3f seconds)" % (query["search_metadata"]["completed_in"])
 
-#-----------------------------------------------------------------------
-# Loop through each of the results, and print its content.
-#-----------------------------------------------------------------------
+    #-----------------------------------------------------------------------
+    # Loop through each of the results, and print its content.
+    #-----------------------------------------------------------------------
 
-f = open('workfile', 'w')
-for result in query["statuses"]:
-	f.write("(%s) @%s %s \n" % (result["created_at"].encode('utf-8'), result["user"]["screen_name"].encode('utf-8'), result["text"].encode('utf-8')))
+    f = open('positives' + str(count), 'w')
+    for result in query["statuses"]:
+        f.write("(%s) @%s %s \n" % (result["created_at"].encode('utf-8'), result["user"]["screen_name"].encode('utf-8'), result["text"].encode('utf-8')))
+        names.write("%s \n" % (result["user"]["screen_name"].encode('utf-8')))
+
+    count += 1
